acc9804daeba2690488dbf8d69f5590094e79a3a
yanimadeitcandleco-cyber <yanimadeitcandleco@gmail.com>
2026-04-27 17:55:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+let productionCosts = {};
+const LOW_THRESHOLD = 5;
+
+async function getToken() {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'client_credentials');
+  params.append('client_id', process.env.SHOPIFY_CLIENT_ID);
+  params.append('client_secret', process.env.SHOPIFY_CLIENT_SECRET);
+  const r = await fetch('https://' + process.env.SHOPIFY_STORE + '/admin/oauth/access_token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: params
+  });
+  const d = await r.json();
+  return d.access_token;
+}
 app.get('/shopify/products', async (req, res) => {
   try {
     const token = await getToken();
